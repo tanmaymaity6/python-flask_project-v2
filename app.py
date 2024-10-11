e252652b060e05dd1a5aa9716ba7ab15a8d6df5a
tanmaymaity <53228036+tanmaymaity6@users.noreply.github.com>
2024-10-11 00:13:49 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from re import DEBUG
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def hello_tangent():
+    return render_template('home.html')
 
 print(__name__)
 if __name__ == "__main__":
