4252b0079745ace4a999c06bfac4840c596f8aba
tanmaymaity <53228036+tanmaymaity6@users.noreply.github.com>
2024-10-18 00:49:10 +0000

    added connection to db

--- app.py
+++ app.py
@@ -1,43 +1,18 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
-JOBS = [
-    {
-        'id': 1,
-        'title': 'Angular Developer',
-        'location': 'San Fransisco, USA',
-        'salary': '$100,000'
-    },
-    {
-        'id': 2,
-        'title': 'React Developer',
-        'location': 'Dallas, USA',
-        'salary': '$200,000'
-    },
-    {
-        'id': 3,
-        'title': 'Devops',
-        'location': 'Boston, USA',
-    },
-    {
-        'id': 4,
-        'title': 'Data Engineer',
-        'location': 'Madison, USA',
-        'salary': '$175,000'
-    },
-]
-
 
 @app.route("/")
 def hello_tangent():
-    return render_template('home.html', jobs=JOBS, company_name='Tangent')
+    jobs = load_jobs_from_db()
+    return render_template('home.html', jobs=jobs, company_name='Tangent')
 
 
 @app.route("/api/jobs")
 def list_jobs():
-    return jsonify(JOBS)
+    jobs=load_jobs_from_db()
+    return jsonify(jobs)
     
-
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
---
+++ database.py
@@ -0,0 +1,52 @@
+from sqlalchemy import create_engine, text
+from pymysql import cursors
+import pymysql
+import os
+
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+timeout = 10
+connection = pymysql.connect(
+    charset="utf8mb4",
+    connect_timeout=timeout,
+    cursorclass=pymysql.cursors.DictCursor,
+    db="defaultdb",
+    host="tangent-careers-tangent-careers.h.aivencloud.com",
+    password=db_connection_string,
+    read_timeout=timeout,
+    port=20931,
+    user="avnadmin",
+    write_timeout=timeout,
+)
+
+def load_jobs_from_db():
+  cursor = connection.cursor()
+  cursor.execute("select * from jobs")
+  jobs=[]
+  for row in cursor.fetchall():
+    jobs.append(dict(row))
+  return jobs
+
+
+
+# try:  
+#   cursor = connection.cursor()
+#   result = cursor.execute("select * from jobs")
+
+###for understanding---
+#   result_dicts=[]
+#   for row in cursor.fetchall():
+#     result_dicts.append(dict(row))
+#   print(result_dicts)
+  
+  # print("type(result):", type(result))
+
+  # result_all = cursor.fetchall()
+  # print("type(result.all()):",type(result_all))
+  # first_result=result_all[0]
+  # print("type(first_result):",type(first_result))
+  # print(first_result)
+
+ #  print(cursor.fetchall())
+ # print("type:type(cursor):", type(cursor))
+# finally:
+#   connection.close()
